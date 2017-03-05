b841ce86af5499ffad149e4ed50d9fe561b72638
Austin Wood <austin@austinkwood.com>
2017-03-05 12:32:45 -0800

    Fix scoping. Add button works, result renders in view

--- calculator.jsx
+++ calculator.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
+class Calculator extends React.Component{
+  constructor(){
+    super();
     this.state = { num1: "", num2: "", result: 0 };
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
+
   }
 
-  setNum1(e) {
+  setNum1(e){
     const num1 = e.target.value ? parseInt(e.target.value) : "";
-    this.setState({ num1 });
+    this.setState({ num1: num1 });
   }
 
-  setNum2(e) {
+  setNum2(e){
     const num2 = e.target.value ? parseInt(e.target.value) : "";
-    this.setState({ num2 });
+    this.setState({ num2: num2 });
   }
 
-  render() {
+  add(e){
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2;
+    this.setState({ result: result });
+  }
+
+  render(){
     const { num1, num2, result } = this.state;
     return (
       <div>
