c75bfd0d6727a1e5840a6225c3c7ce103d409578
Austin Wood <austin@austinkwood.com>
2017-03-05 12:15:52 -0800

    Add input fields and save state

--- calculator.jsx
+++ calculator.jsx
@@ -6,12 +6,23 @@ class Calculator extends React.Component {
     this.state = { num1: "", num2: "", result: 0 };
   }
 
-  // your code here
+  setNum1(e) {
+    const num1 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num1 });
+  }
+
+  setNum2(e) {
+    const num2 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num2 });
+  }
 
   render() {
+    const { num1, num2, result } = this.state;
     return (
       <div>
         <h1>{this.state.result}</h1>
+        <input onChange={this.setNum1} value={num1}/>
+        <input onChange={this.setNum2} value={num2}/>
       </div>
     );
   }
